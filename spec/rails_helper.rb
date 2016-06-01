a1ca779c4a9b54640998c64454a59b7573114bad
Roger Rodriguez <evolpirate@gmail.com>
2016-06-01 18:18:13 -0400

    adding test to verify authentication and authorization test failing now & add it landing page add it devise views to customize

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Features::SessionHelpers, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
