4ada2c4e211098ddbaacdcd4cef3e0da0bdd9d57
Brian Kim <bkim8815@gmail.com>
2016-06-05 22:13:59 -0400

    practice model spec modified

--- spec/models/practice_spec.rb
+++ spec/models/practice_spec.rb
@@ -14,10 +14,14 @@ RSpec.describe Practice, type: :model do
     let! (:course) {FactoryGirl.create(:course, user: user)}
     let! (:chapter) {FactoryGirl.create(:chapter, course: course)}
     let! (:lesson) {FactoryGirl.create(:lesson, chapter: chapter)}
-    let! (:practice) {FactoryGirl.create(:practice, lesson: lesson, user: user)}
+    let! (:trainee) {FactoryGirl.create(:user)}
+    let! (:practice) {FactoryGirl.create(:practice, lesson: lesson, user: trainee)}
 
       it 'should lesson id when created under lesson' do
         expect(practice.lesson).to eq lesson
+      end
+      it 'should not have conflict in user id' do
+        expect(practice.user).to eq trainee
       end
   end
 end
