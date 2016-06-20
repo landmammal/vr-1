e8c898aff7f71cc2168dbd6ece55092e1ee85ad5
Roger Rodriguez <evolpirate@gmail.com>
2016-06-20 02:05:51 -0400

    fixing welcome/index syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource)
-		request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+		# request.env['omniauth.origin'] || stored_location_for(resource) || root_path
 	end
 	# al
 
@@ -22,12 +22,12 @@ class ApplicationController < ActionController::Base
     # al--->
 	protected
 
-	def authenticate_user!
-		if user_signed_in?
-		  super
-		else
-		  redirect_to root_path
-		end
-	end
+	# def authenticate_user!
+	# 	if user_signed_in?
+	# 	  super
+	# 	else
+	# 	  redirect_to root_path
+	# 	end
+	# end
 	# al
 end
