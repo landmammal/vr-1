43cec243449e8e305c4ba07f59fe62f70530b420
Roger Rodriguez <evolpirate@gmail.com>
2016-06-23 20:28:33 -0400

    adding editing tools for user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :age, :race, :company, :education, :email, :password, :password_confirmation, :role, :banner, :profile)
+    params.require(:user).permit(:first_name, :last_name, :age, :race, :company, :education, :email, :password, :password_confirmation, :role, :banner, :profile, :current_password)
   end
 end
