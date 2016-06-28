87a2e675daf68de3d54631ee2f095668f09ac53f
Roger Rodriguez <evolpirate@gmail.com>
2016-06-27 21:30:12 -0400

    associationg course creation with current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,11 @@ class User < ActiveRecord::Base
 
   def photo
       user_profile = profile_file_name
-      user_profile.present? ? user_profile : '/assets/default_user.png'
+      user_profile.present? ? profile.url(:square) : '/assets/default_user.png'
   end
-  def top_banner 
+  def top_banner
       user_banner = banner_file_name
-      user_banner.present? ? user_banner : '/assets/banner.jpg'
+      user_banner.present? ? banner.url(:medium) : '/assets/banner.jpg'
     # user_info.banner.present? ? user_info.banner : '/uploads/user_info/banner/default.jpg'
   end
 
