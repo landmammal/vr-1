84703fa34c89b6c53cdae8209d6b39a7063c857c
Roger Rodriguez <evolpirate@gmail.com>
2016-08-25 21:13:18 -0500

    started adding the topics of the course in a course partial/ found topic not saving in rehearsal object

--- app/controllers/rehearsals_controller.rb
+++ app/controllers/rehearsals_controller.rb
@@ -56,6 +56,7 @@ class RehearsalsController < ApplicationController
   end
 
   private
+
   def set_topic
 
     @topic = Topic.find(params[:topic_id])
@@ -71,7 +72,7 @@ class RehearsalsController < ApplicationController
   end
 
   def rehearsal_params
-    params.require(:rehearsal).permit(:course_id, :lesson_id, :group_id, :token, :video_token, :trainee_id, :script, :submission)
+    params.require(:rehearsal).permit(:course_id, :lesson_id, :group_id, :token, :video_token, :trainee_id, :script, :submission, :topic_id)
   end
 
   def set_lesson
