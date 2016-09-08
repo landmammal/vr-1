a7a0398ca7252d661f5076cc979647d7683cfe6c
Roger Rodriguez <evolpirate@gmail.com>
2016-09-07 23:25:32 -0500

    minor changes to the admail mail template

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,7 @@ class AdminMailer < ActionMailer::Base
 
   def new_user_waiting_for_approval
     # @user = admin
-    @url  = 'http://videorehearser.com/login'
+    @url  = 'https://videorehearser.herokuapp.com/users/sign_in'
     mail(to: 'carlos@videorehearser.com', subject: "New User Signup")
   end
 end
