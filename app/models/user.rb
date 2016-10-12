e9a1d5abce6f1a5a4441eee2f471f45ceb08a3d0
Roger Rodriguez <evolpirate@gmail.com>
2016-10-12 12:40:59 -0500

    username uniqness and validation add it

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true
+  validates :username, presence: true
+  validates_uniqueness_of :username
 
   # checking for approved in user field
   def active_for_authentication?
