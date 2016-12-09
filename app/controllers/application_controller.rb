60b4cf7bb419f1fe3b6705acc784e42123dde7a7
Al Delcy <aldelcy2000@gmail.com>
2016-12-08 22:05:35 -0500

    setup SSL with Heroku and forcing https protocole

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+
+  def self.force_ssl(options = {})
+    host = options.delete(:host)
+    before_filter(options) do
+      if !request.ssl? && !Rails.env.development? && !(respond_to?(:allow_http?) && allow_http?)
+        redirect_options = {:protocol => 'https://', :status => :moved_permanently}
+        redirect_options.merge!(:host => host) if host
+        redirect_options.merge!(:params => request.query_parameters)
+        redirect_to redirect_options
+      end
+    end
+  end
+
+  force_ssl
+
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :null_session
 
