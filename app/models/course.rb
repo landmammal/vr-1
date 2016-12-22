311acca1002c0ac6b042762b21cdc5d134e6b9e5
Roger Rodriguez <evolpirate@gmail.com>
2016-12-21 22:33:49 -0500

    created method t delete all association tables linked to lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,4 +16,26 @@ class Course < ApplicationRecord
   	[[:Public, 0],[:Unpublished, 1],[:Paid, 2],[:Private, 3]]
   end
 
+  def delete_associations
+  # delete all registrations to this course
+  CourseRegistration.where(course_id: self).delete_all
+  GroupRegistration.where(course_id: self).delete_all
+
+  # grab all rehearsals inside this course
+  rehearsals = Rehearsal.where(course_id: self)
+
+  # if there is any delete them from performance feedback table
+  rehearsals.each { |r| PerformanceFeedback.where(rehearsal_id: r).delete_all } if rehearsals.any?
+
+  # grab all lessons inside this course
+  self.topics.each { |t| @lessons = Lesson.where(instructor_id: self.instructor, topic_id: t) }
+
+  # check if there are any and if so, delete all rehearsals attach to them on lessons rehearsals table
+  @lessons.each { |l| LessonRehearsal.where(lesson_id: l,).delete_all } if @lessons.any?
+
+  # now that the rehearsals is disconnected from other tables
+  # we can delete all rehearsals in this course
+  # and be able to delete the course
+  Rehearsal.where(course_id: self).delete_all
+  end
 end
