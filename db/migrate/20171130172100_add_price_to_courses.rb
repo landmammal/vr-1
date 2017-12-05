3ec329c33327567343ff4fa5eb1800d74f37b0ab
Al Delcy <aldelcy@users.noreply.github.com>
2017-12-05 02:21:53 -0500

    Update 20171130172100_add_price_to_courses.rb

--- db/migrate/20171130172100_add_price_to_courses.rb
+++ db/migrate/20171130172100_add_price_to_courses.rb
@@ -1,5 +1,5 @@
 class AddPriceToCourses < ActiveRecord::Migration[5.0]
   def change
-    add_column :courses, :price, :integer
+    add_column :courses, :price, :decimal, precision: 10, scale: 2
   end
 end
