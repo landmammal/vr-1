431698643e7f14d376b9a2d300a57783fecc3558
Al Delcy <aldelcy2000@gmail.com>
2018-02-22 19:17:38 -0500

    separated migrations

---
+++ db/migrate/20180216030405_add_refnum_to_everything.rb
@@ -0,0 +1,20 @@
+class AddRefnumToEverything < ActiveRecord::Migration[5.0]
+  def change
+    add_column :courses, :color, :string
+    add_column :courses, :requirements, :text
+    add_column :courses, :short_desc, :text
+    add_column :courses, :topics_order, :array
+    add_column :topics, :lessons_order, :array
+    
+    # TODO: REF NUMBERS
+    add_column :courses, :refnum, :array
+    add_column :topics, :refnum, :array
+    add_column :lessons, :refnum, :array
+    add_column :explanations, :refnum, :array
+    add_column :prompts, :refnum, :array
+    add_column :models, :refnum, :array
+    add_column :feedbacks, :refnum, :array
+    add_column :rehearsals, :refnum, :array
+    add_column :concepts, :refnum, :array
+  end
+end
